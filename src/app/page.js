e86c7270ba11ec5787cba5933e3b6d639543aa4d
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-03-27 15:31:07 +0100

    started work on the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-slate-500 w-full h-[100px]">
-      
+    <div className="bg-[#003f51] w-full h-[400px] flex justify-center items-center flex-col">
+      <p className="text-[#00fdff] font-bold font-mono text-[25px] italic">Hello world</p>
     </div>
   );
 }
