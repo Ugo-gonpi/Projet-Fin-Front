885900268c279eda2636c836d5cb5df1298ed596
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-03-29 11:35:41 +0100

    created the cards, did most of their design, only need the images left

--- src/app/acceuil/page.js
+++ src/app/acceuil/page.js
@@ -1,13 +1,33 @@
-"use client"
-
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
-export default function Acceuil() {
+async function getProduct(){
+    const res = await fetch('https://www.freetogame.com/api/games')
+    return res.json()
+}
+export default async function Acceuil() {
+    const products = await getProduct()
 
     return(
-        <div className="bg-[#003f51] w-full h-[500px] flex justify-center items-center flex-col">
-            
+        <div className="bg-[#003f51] w-full justify-center items-center flex flex-wrap">
+            {
+                products.map((element, key) => {
+                    return(
+                        <div className="w-[300px] h-[500px] flex justify-center items-center flex-wrap" key={key}>
+                            <div className="bg-[#007f8e] w-[300px] h-[440px] text-center rounded-[10px] hover:scale-105 transition-[2s] border-2 border-[#00aeb8] ml-5 shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)]">
+                                <div className="w-full h-[55%]">
+
+                                </div>
+
+                                <div className="w-full h-[45%]">
+                                    <p className="text-[#00fdff] font-[fontSite] underline text-[18px] font-bold">{element.title}</p>
+                                    <p className="text-[#00fdff] font-[fontSite]">-Publisher: {element.publisher}</p>
+                                    <p className="text-[#00fdff] font-[fontSite]">-Developer: {element.developer}</p>
+                                    {/* <p className="text-[#00fdff] font-[fontSite] underline">{element.release_date}</p> */}
+                                    <button className="mt-[10px] w-[150px] h-[40px] border-[#00fdff] border-2 rounded-[10px] text-[#00fdff] bg-[#003f51] font-[fontSite] hover:brightness-125 active:brightness-150 font-bold">Details</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function NotFound() {
     
     return(
-        <div className="w-full h-full flex justify-center items-center flex-col bg-[#003f51]">
+        <div className="w-full h-[665px] flex justify-center items-center flex-col bg-[#003f51]">
 
-            <h1 className="font-bold text-[30px] text-[#00fdff]">Oh oh, une erreur c'est produite</h1>
+            <h1 className="font-bold text-[30px] text-[#00fdff] underline">Oh oh, une erreur c'est produite</h1>
 
-            <p className="font-bold text-[20px] text-[#00fdff]">Veuillez vérifeir que vous avez bien écrit vos informations dans le login</p>
+            <p className="font-bold text-[20px] text-[#00fdff]">Veuillez vérifier que vous avez écrits les bonnes informations dans le login</p>
 
-            <Link href="/" className="mt-4 underline text-[#00fdff]">Retour aux login</Link>
+            <Link href="/" className="mt-4 underline text-[#00fdff]">Retour au login</Link>
 
         </div>
     )
