98b65950cccf6b98ae9a0cc6dae704504203d78b
Ugo-gonpi <ugogonzalez50@gmail.com>
2024-03-29 15:10:10 +0100

    created the footer

--- src/app/acceuil/layout.js
+++ src/app/acceuil/layout.js
@@ -1,5 +1,12 @@
 import Link from "next/link"
+
 import Image from "next/image"
+
+import dicord from "./../assets/discord.png"
+import facebook from "./../assets/facebook.png"
+import gmail from "./../assets/gmail.png"
+import instagram from "./../assets/instagram.png"
+import linkedin from "./../assets/linkedin.png"
 
 export default function AcceuilLayout({ children }) {
     return(
@@ -11,13 +18,17 @@ export default function AcceuilLayout({ children }) {
 
                 </div>
 
-                <div className="w-[80%] h-full flex justify-around items-center">
+                <div className="w-[60%] h-full flex justify-around items-center">
 
-                    <Link href="/acceuil" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff]">Acceuil</Link>
+                    <Link href="/acceuil" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff] text-[20px]">Acceuil</Link>
 
-                    <Link href="/genres" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff]">Types</Link>
+                    <Link href="/genres" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff] text-[20px]">Genre de jeux</Link>
 
-                    <Link href="/details" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff]">Details</Link>
+                    <Link href="/details" className="text-[#00fdff] font-bold font-[fontSite] hover:border-b-2 hover:border-[#00fdff] text-[20px]">Details</Link>
+
+                </div>
+
+                <div className="w-[20%] h-full">
 
                 </div>
 
@@ -27,12 +38,17 @@ export default function AcceuilLayout({ children }) {
 
             <footer className="w-full h-[150px] bg-[#007f8e] flex justify-center items-center flex-col">
 
-                <div>
-                    <p className="text-[#00fdff] font-[fontSite]">Hello world, this is the footer</p>
+                <div className="w-full h-[70%] flex justify-around items-center flex-row">
+                    <a href=""><Image src={dicord} alt="image de discord" width={40} height={40}></Image></a>
+                    <a href="https://www.facebook.com/ugo.gonzalezpinto"><Image src={facebook} alt="image de facebook" width={40} height={40}></Image></a>
+                    <a href=""><Image src={gmail} alt="image de gmail" width={40} height={40}></Image></a>
+                    <a href="https://www.instagram.com/ugo.gp_2/"><Image src={instagram} alt="image de discord" width={40} height={40}></Image></a>
+                    <a href="https://www.linkedin.com/in/ugo-gonzalez-pinto-9aa3062b5/"><Image src={linkedin} alt="image de discord" width={40} height={40}></Image></a>
                 </div>
 
-                <div>
-                    <p className="font-[fontSite]">Bruh</p>
+                <div className="w-full h-[30%] flex justify-center items-center flex-col">
+                    <p className="font-[fontSite] text-[#00fdff] brightness-75">Tous droits de ce site reviene à :</p>
+                    <p className="font-[fontSite] text-[#00cdc9] brightness-75 text-[14px]">Ugo Gonzalez Pinto ©</p>
                 </div>
 
             </footer>
--- src/app/acceuil/page.js
+++ src/app/acceuil/page.js
@@ -14,7 +14,7 @@ export default async function Acceuil() {
                 products.map((element, key) => {
                     return(
                         <div className="w-[300px] h-[500px] flex justify-center items-center flex-wrap" key={key}>
-                        
+
                             <div className="bg-[#007f8e] w-[300px] h-[420px] text-center rounded-[10px] hover:scale-105 transition-[2s] border-2 border-[#00aeb8] ml-5 shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)]">
 
                                 <div className="w-full h-[55%]">
@@ -23,11 +23,11 @@ export default async function Acceuil() {
 
                                 <div className="w-full h-[45%] px-4 flex justify-between items-center flex-col">
 
-                                    <p className="text-[#00fdff] font-[fontSite] underline text-[18px] font-bold">{element.title}</p>
+                                    <p className="text-[#00fdff] font-[fontSite] text-[18px] font-bold">{element.title}</p>
 
-                                    <p className="text-[#00fdff] font-[fontSite]"> <span className="text-[#00fdff] font-[fontSite] underline">-Publisher:</span> {element.publisher}</p>
+                                    <p className="text-[#00fdff] font-[fontSite]"> <span className="text-[#00fdff] font-[fontSite] font-bold">-Publisher :</span> {element.publisher}</p>
 
-                                    <p className="text-[#00fdff] font-[fontSite]"> <span className="text-[#00fdff] font-[fontSite] underline">-Developer:</span> {element.developer}</p>
+                                    <p className="text-[#00fdff] font-[fontSite]"> <span className="text-[#00fdff] font-[fontSite] font-bold">-Developer :</span> {element.developer}</p>
 
                                     <Link className="mt-[10px] w-[150px] h-[40px] border-[#00fdff] border-2 rounded-[10px] text-[#00fdff] bg-[#003f51] font-[fontSite] hover:brightness-125 active:brightness-150 flex justify-center items-center font-bold mb-[10px]" href="/details">Details</Link>
 
